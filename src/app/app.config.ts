1d651cda79c29312e91bc720a7fffe761db2ca9c
Davide Brignoli <68071016+AtiposEiji@users.noreply.github.com>
2024-07-18 10:08:12 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
-
+import { HashLocationStrategy, LocationStrategy  } from '@angular/common';
 import {routes} from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 
